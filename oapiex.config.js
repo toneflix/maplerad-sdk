f4b236006580b9ae7e8b302aa5cc9b60f6b46f01
3m1n3nc3 <mygames.ng@gmail.com>
2026-04-04 13:26:19 +0100

    refactor: update .gitignore to include oapiex.config.js and remove cache directory

--- oapiex.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@oapiex/sdk-kit'
-/**
- * See https://toneflix.github.io/oapiex/reference/configuration for docs
- */
-export default defineConfig({
-  clientSecret: process.env.MAPLERAD_CLIENT_SECRET,
-  environment: 'sandbox',
-  debugLevel: 0,
-})
